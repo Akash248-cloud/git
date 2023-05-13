12560a3fd6c65ec6a1fbf41b65c4a8fb775cad07
coding-devel <akashkumar08623@gmail.com>
2023-05-13 20:27:00 +0530

    intial commit

--- sum.java
+++ sum.java
@@ -3,6 +3,6 @@ class sum
     public static void main(String[] args) {
         int a=20, b=45, c;
         c=a+b;
-        System.out.println("the assing two numbers: "+c);
+        System.out.println("the adding two numbers: " +c);
     }
 }

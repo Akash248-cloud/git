fc19f66a0471a2f2b8ffc0e788b409ab223f38cd
coding-devel <akashkumar08623@gmail.com>
2023-05-15 14:33:19 +0530

    initialcode

--- sum.java
+++ sum.java
@@ -1,8 +1,18 @@
 class sum 
 {
-    public static void main(String[] args) {
+
+    public void add()
+    {
         int a=20, b=45, c;
         c=a+b;
-        System.out.println("the adding two numbers: " +c);
+        System.out.println("The adding two numbers: " +c);
+        link="a.html ";
     }
-}
+
+
+    public static void main(String[] args)
+     {
+        sum s = new sum();
+        s.add();
+     }
+ }

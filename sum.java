7bb252c7fa86ae944920f33afef5f10a453bc441
Akash248-cloud <akashkumar46085@gmail.com>
2023-05-16 11:46:11 +0530

    update code

--- sum.java
+++ sum.java
@@ -3,9 +3,11 @@ class sum
 
     public void add()
     {
-        int a=20, b=45, c;
+        int a=20, b=45, c,d;
         c=a+b;
+        d=a*b;
         System.out.println("The adding two numbers: " +c);
+        System.out.println("The multiplying two numbers: " +d);
         //link="a.html ";
     }
 
